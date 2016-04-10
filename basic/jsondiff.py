581c9897f67f27cc4ca84f3ef285b3de1108015f
shuqin <shuqin_1984@163.com>
2016-04-10 12:24:13 +0800

    improve jsondiff.py: add parseKeys and add test data file

--- basic/jsondiff.py
+++ basic/jsondiff.py
@@ -41,57 +41,106 @@ def diffKeys(json1, json2):
     keys2 = parseKeys(json2)
     keyset1 = set(keys1)
     keyset2 = set(keys2)
-    print "keys in first json, not in second json: "
-    print keyset1.difference(keyset2) 
+    return keyset1.difference(keyset2) 
 
-def cmpArray(jsonArr1, jsonArr2, f_res):
-    for i in range(0, len(jsonArr1)):
-        diffDict(jsonArr1[i], jsonArr2[i], f_res)
 
-def cmpPrimitive(key, value1, value2, f_res):
+def cmpArray(jsonArr1, jsonArr2, diff, prefix_key):
+    '''
+       need to be improved
+    '''
+    arrlen1 = len(jsonArr1)
+    arrlen2 = len(jsonArr2)
+    minlen = min(arrlen1, arrlen2)
+    if arrlen1 != arrlen2:
+        diff.append((prefix_key+'.length', arrlen1, arrlen2))
+    for i in range(0, minlen):
+        diffDict(jsonArr1[i], jsonArr2[i], diff, prefix_key)
+
+def cmpPrimitive(key, value1, value2, diff, prefix_key):
 
     if isinstance(value1,list) or isinstance(value1, dict) \
        or isinstance(value2, list) or isinstance(value2, dict):
        return
 
-    if value1 == value2:
-       f_res.write(key + " " + str(value1) + " OK\n")
-    else:
-       f_res.write(key + " " + str(value1) + " " + str(value2) + " NotEqual\n") 
+    if value1 != value2:
+       diff.append((prefix_key + key, str(value1), str(value2))) 
 
 
-def diffDict(json1, json2, f_res):
+def diffDict(json1, json2, diff, prefix_key):
+
+    if prefix_key != '':
+        prefix_key = prefix_key+'.'
 
     for (key, value) in json1.iteritems():
         json2Value = json2.get(key)
         #print "key: ", key, ", value: ", value, " , value2: ", json2Value
+
         if json2Value == None:
-            f_res.write(key + " in json1, not in json2 " + "\n")
+            diff.append((prefix_key + key, value, None))
 
         if isinstance(value, dict) and isinstance(json2Value, dict):
-            diffDict(value, json2Value, f_res)        
+            diffDict(value, json2Value, diff, prefix_key + key)        
  
         if isinstance(value, list) and isinstance(json2Value, list):
-            cmpArray(value, json2Value, f_res)   
+            cmpArray(value, json2Value, diff, prefix_key + key)   
 
-        cmpPrimitive(key, value, json2Value, f_res)    
+        cmpPrimitive(key, value, json2Value, diff, prefix_key)    
  
 
-def diffJson(json1, json2, f_res):
-    diffDict(json1, json2, f_res)
+def diffJson(json1, json2):
+    jsondiff = list()
+    diffDict(json1, json2, jsondiff, '')
+    return jsondiff
+ 
+
+def diffJsonToFile(filename, json1, json2):
+    f_res = open(filename, 'w')
+    diff_res = diffJson(json1, json2)
+    for diff in diff_res:
+        (key,v1,v2) = diff
+        if v2 is None:
+            f_res.write('key %s in json1 not in json2. \n' % key)
+        else:
+            f_res.write('key %s in json1 = %s yet in json2 = %s. \n' %(key, v1, v2))
+
     f_res.close()
 
 
+def tesParsetKeysSingle(jsonobj, expected):
+    assert set(parseKeys(jsonobj)) == set(expected)
+    
+def testParseKeys():
+    for v in ({}, [], "good", 1, 3.14, -2.71, -1, 0.1, 2.71E3, 2.71E+3, 2.71E-32, 2.71e3, 2.71e+3, 2.71e-32, True, False, None, "null\n\\\"\/\b\f\n\r\t\u"):
+        tesParsetKeysSingle(parseKeys(v), [])
+    tesParsetKeysSingle({"code": 200}, ['code'])
+    tesParsetKeysSingle({"code": 200, "msg": "ok", "list": [], "extra":{}}, ['code', 'msg', 'list', 'extra'])
+    tesParsetKeysSingle({"code": 200, "msg": "ok", "list": [{"id": 20, "no":"115"}], "extra":{"size": 20, "info": {"owner": "qin"}}}, ['code', 'msg', 'list', 'list..id', 'list..no', 'extra', 'extra.size', 'extra.info', 'extra.info.owner'])
+    tesParsetKeysSingle({'msg': 'ok', 'code': 200, 'list': [{'items': [{'price': 21, 'infos': {'feature': ''}, 'name': 'n1'}], 'id': 20, 'no': '1000020'}], 'metainfo': {'total': 20, 'info': {'owner': 'qinshu', 'parts': [{'count': 13, 'time': {'start': 1230002456, 'end': 234001234}}]}}}, ['msg', 'code', 'list', 'list..items', 'list..items..price', 'list..items..infos', 'list..items..infos.feature', 'list..items..name','list..id', 'list..no',  'metainfo', 'metainfo.total', 'metainfo.info', 'metainfo.info.owner', 'metainfo.info.parts', 'metainfo.info.parts..count', 'metainfo.info.parts..time' ,'metainfo.info.parts..time.start', 'metainfo.info.parts..time.end']) 
+
+    print 'testPassed'
+
+
+def test():
+    testParseKeys()
+
+
 if __name__ == "__main__":
-    content1 = readFile("/Users/shuqin/Data/order_list_data.txt")
-    content2 = readFile("/Users/shuqin/Data/pf_list_data.txt")
+    
+    test()
+
+    content1 = readFile("../data/json_data_v1.txt")
+    content2 = readFile("../data/json_data_v2.txt")
     json1 = json.loads(content1)
     json2 = json.loads(content2)
-    diffJson(json1, json2, open('diff_waptopf.txt', 'w'))
-    diffJson(json2, json1, open('diff_pftowap.txt', 'w'))
-   
-    print "keys in json1: "
-    print parseKeys(json1) 
+
+    print "keys in json_data_v2: "
+    print parseKeys(json2)
+            
+    print 'keys in json_data_v1 yet not in json_data_v2: '
+    print diffKeys(json1, json2)
+                        
+    print 'keys in json_data_v2 yet not in json_data_v1: '
+    print diffKeys(json2, json1)
     
-    diffKeys(json1, json2)
-    diffKeys(json2, json1)
+    diffJsonToFile('../data/diff_v1tov2.txt', json1, json2)
+    diffJsonToFile('../data/diff_v2tov1.txt', json2, json1) 
