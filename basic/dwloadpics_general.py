83c7b67def84dfa558e9712d2dac4c6217a56107
shuqin <shuqin_1984@163.com>
2016-10-28 00:13:58 +0800

    improve general tool for download pictures from network

--- basic/dwloadpics_general.py
+++ basic/dwloadpics_general.py
@@ -1,17 +1,34 @@
-#_*_encoding:utf-8_*_
 #!/usr/bin/python
+#_*_encoding:utf-8_*_
 
 import os
 import re
 import sys
+import json
 from multiprocessing import (cpu_count, Pool)
 from multiprocessing.dummy import Pool as ThreadPool
 
+import argparse
 import requests
 from bs4 import BeautifulSoup
 
 ncpus = cpu_count()
 saveDir = os.environ['HOME'] + '/joy/pic/test'
+
+def parseArgs():
+    description = '''This program is used to batch download pictures from specified urls.
+                     eg python dwloadpics_general.py -u http://xxx.html -g 1 10 _p -r '[{"img":["jpg"]}, {"class":["picLink"]}, {"id": ["HidenDataArea"]}]'
+                     will search and download pictures from network urls http://xxx_p[1-10].html  by specified rulePath
+                  '''
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-u','--url', nargs='+', help='At least one html urls are required', required=True)
+    parser.add_argument('-g','--generate',nargs=2, help='Given range containing two number (start end) to generate more htmls if not empty ', required=False)
+    parser.add_argument('-r','--rulepath',nargs=1,help='rule path to search pictures. if not given, search pictures in given urls', required=False)
+    args = parser.parse_args()
+    init_urls = args.url
+    gene = args.generate
+    rulepath = args.rulepath
+    return (init_urls, gene, rulepath)
 
 def createDir(dirName):
     if not os.path.exists(dirName):
@@ -32,15 +49,15 @@ class IoTaskThreadPool(object):
     '''
     def __init__(self, poolsize):
         self.ioPool = ThreadPool(poolsize)
-    
+
     def execTasks(self, ioFunc, ioParams):
         if not ioParams or len(ioParams) == 0:
             return []
         return self.ioPool.map(ioFunc, ioParams)
 
-    def execTasksAsync(self, ioFunc, ioParams):      
-        if not ioParams or len(ioParams) == 0:  
-            return []                           
+    def execTasksAsync(self, ioFunc, ioParams):
+        if not ioParams or len(ioParams) == 0:
+            return []
         self.ioPool.map_async(ioFunc, ioParams)
 
     def close(self):
@@ -53,16 +70,16 @@ class TaskProcessPool():
     '''
        process pool for cpu operations or task assignment
     '''
-    def __init__(self):           
+    def __init__(self):
         self.taskPool = Pool(processes=ncpus)
-                                  
+
     def addDownloadTask(self, entryUrls):
         self.taskPool.map_async(downloadAllForAPage, entryUrls)
-                                  
-    def close(self):              
-        self.taskPool.close()     
-                                  
-    def join(self):               
+
+    def close(self):
+        self.taskPool.close()
+
+    def join(self):
         self.taskPool.join()
 
 def getHTMLContentFromUrl(url):
@@ -82,6 +99,43 @@ def batchGrapHtmlContents(urls):
     global grapHtmlPool
     return grapHtmlPool.execTasks(getHTMLContentFromUrl, urls)
 
+def getAbsLink(link):
+    global serverDomain
+
+    try:
+        href = link.attrs['href']
+        if href.startswith('/'):
+            return serverDomain + href
+        else:
+            return href
+    except:
+        return ''
+
+def getTrueImgLink(imglink):
+    '''
+    get the true address of image link:
+        (1) the image link is http://img.zcool.cn/community/01a07057d1c2a40000018c1b5b0ae6.jpg@900w_1l_2o_100sh.jpg
+            but the better link is http://img.zcool.cn/community/01a07057d1c2a40000018c1b5b0ae6.jpg (removing what after @) 
+        (2) the image link is relative path /path/to/xxx.jpg
+            then the true link is serverDomain/path/to/xxx.jpg serverDomain is http://somedomain
+    '''
+
+    global serverDomain
+    try:
+        href = imglink.attrs['src']
+        if href.startswith('/'):
+            href = serverDomain + href
+        pos = href.find('jpg@')
+        if pos == -1:
+            return href
+        return href[0: pos+3] 
+    except:
+        return ''
+
+def batchGetImgTrueLink(imgLinks):
+    hrefs = map(getTrueImgLink, imgLinks)
+    return filter(lambda x: x!='', hrefs)
+
 def findWantedLinks(htmlcontent, rule):
     '''
        find html links or pic links from html by rule.
@@ -90,31 +144,39 @@ def findWantedLinks(htmlcontent, rule):
           (2) a link with class=[value1,value2,...]
           (3) img with src=xxx.jpg|png|...
        a rule is map containing sub rule such as:
-          {
-              'id': [id1, id2, ..., idn],
-              'class': [c1, c2, ..., cn],
-              'img': ['jpg', 'png', ... ]
-          }
-                 
+          { 'id': [id1, id2, ..., idn] } or
+          { 'class': [c1, c2, ..., cn] } or
+          { 'img': ['jpg', 'png', ... ]}
+
     '''
 
     soup = BeautifulSoup(htmlcontent, "lxml")
     alinks = []
     imglinks = []
+
     for (key, values) in rule.iteritems():
         if key == 'id':
             for id in values:
-                alinks.extend(soup.find_all('a', id=id))
+                links = soup.find_all('a', id=id)
+                links = map(getAbsLink, links)
+                links = filter(lambda x: x !='', links)
+                alinks.extend(links)
         elif key == 'class':
             for cls in values:
-                alinks.extend(soup.find_all('a', class_=cls))
+                if cls == '*':
+                    links = soup.find_all('a')
+                else:    
+                    links = soup.find_all('a', class_=cls)
+                links = map(getAbsLink, links)
+                links = filter(lambda x: x !='', links)
+                alinks.extend(links)        
         elif key == 'img':
             for picSuffix in values:
                 imglinks.extend(soup.find_all('img', src=re.compile(picSuffix)))
-    
+
     allLinks = []
-    allLinks.extend(map(lambda link: link.attrs['href'], alinks))
-    allLinks.extend(map(lambda img: img.attrs['src'], imglinks))
+    allLinks.extend(alinks)
+    allLinks.extend(batchGetImgTrueLink(imglinks))
     return allLinks
 
 def batchGetLinksByRule(htmlcontentList, rule):
@@ -130,17 +192,13 @@ def batchGetLinksByRule(htmlcontentList, rule):
 def defineResRulePath():
     '''
         return the rule path from init htmls to the origin addresses of pics
-        if we find the origin addresses of pics by 
-        init htmls --> grap htmlcontents --> rules1 --> intermediate htmls 
+        if we find the origin addresses of pics by
+        init htmls --> grap htmlcontents --> rules1 --> intermediate htmls
            --> grap htmlcontents --> rules2 --> intermediate htmls
            --> grap htmlcontents --> rules3 --> origin addresses of pics
-        we say the rulepath is [rules1, rules2, rules3] 
+        we say the rulepath is [rules1, rules2, rules3]
     '''
     return []
-
-def definePconlineResRulePath():
-    return [{'class': ['picLink']}, {'class':['aView aViewHD']}, {'img': ['jpg']}]
-
 
 def findOriginAddressesByRulePath(initUrls, rulePath):
     '''
@@ -152,21 +210,19 @@ def findOriginAddressesByRulePath(initUrls, rulePath):
         links = batchGetLinksByRule(htmlContents, rule)
         result = []
         result.extend(links)
+        result = filter(lambda link: link.startswith('http://'),result)    
+    print result
     return result
 
-def downloadFromPconline(serial_num, start, end):
-    entryUrl = 'http://dp.pconline.com.cn/list/all_t%d_p%d.html'
-    entryUrls = [ (entryUrl % (serial_num, ind)) for ind in range(start, end+1)]
-    rulePath = definePconlineResRulePath()
-    picOriginAddresses = findOriginAddressesByRulePath(entryUrls, rulePath)
-
+def downloadFromUrls(initUrls, rulePath):
     global dwPicPool
+    picOriginAddresses = findOriginAddressesByRulePath(initUrls, rulePath)
     dwPicPool.execTasksAsync(downloadPic, picOriginAddresses)
 
-@catchExc 
+@catchExc
 def downloadPic(picsrc):
     '''
-       download pic from pic href such as 
+       download pic from pic href such as
             http://img.pconline.com.cn/images/upload/upc/tx/photoblog/1610/21/c9/28691979_1477032141707.jpg
     '''
 
@@ -175,18 +231,18 @@ def downloadPic(picsrc):
 
     picr = requests.get(picsrc, stream=True)
     with open(saveFile, 'wb') as f:
-        for chunk in picr.iter_content(chunk_size=1024):  
+        for chunk in picr.iter_content(chunk_size=1024):
             if chunk:
                 f.write(chunk)
-                f.flush() 
+                f.flush()
     f.close()
 
-def divideNParts(total, N):                                                                                                                          
+def divideNParts(total, N):
     '''
        divide [0, total) into N parts:
         return [(0, total/N), (total/N, 2M/N), ((N-1)*total/N, total)]
     '''
-     
+
     each = total / N
     parts = []
     for index in range(N):
@@ -198,26 +254,66 @@ def divideNParts(total, N):
         parts.append((begin, end))
     return parts
 
-def testBatch():
+def testBatchGetLinks():
     urls = ['http://dp.pconline.com.cn/list/all_t145.html', 'http://dp.pconline.com.cn/list/all_t292.html']
     htmlcontentList = map(getHTMLContentFromUrl, urls)
-    rules = {'img':['jpg'], 'class':['picLink'], 'id': ['HidenDataArea']}
+    rules = {'class':['picLink'], 'id': ['HidenDataArea'], 'img':['jpg']}
     allLinks = batchGetLinksByRule(htmlcontentList, rules)
-    for link in allLinks:                                  
+    for link in allLinks:
         print link
+
+def generateMoreInitUrls(init_urls, gene):
+    '''
+      Generate more initial urls using init_urls and a range specified by gene
+      to generate urls, we give a base url containing a placeholder, then replace placeholder with number.
+       eg. 
+       base url:  http://xxx.yyy?k1=v1&k2=v2&page=placeholder -> http://xxx.yyy?k1=v1&k2=v2&page=[start-end]
+       base url is specified by -u option if -g is given.
+    '''
+
+    if not gene:
+        return init_urls
+
+    start = int(gene[0])
+    end = int(gene[1])
+    truerange = map(lambda x: x+start, range(end-start+1))
+    resultUrls = []
+    for ind in truerange:
+        for url in init_urls:
+            resultUrls.append(url.replace('placeholder', str(ind)))
+    return resultUrls
+
+def parseRulePathParam(rulepathjson):
+    rulepath = [{'img': ['jpg', 'png']}]
+    if rulepathjson:
+        try:
+            rulepath = json.loads(rulepathjson[0])   
+        except ValueError as e:
+            print 'Param Error: invalid rulepath %s %s' % (rulepathjson, e)
+            sys.exit(1) 
+    return rulepath
+
+def parseServerDomain(url):
+    parts = url.split('/',3)
+    return parts[0] + '//' + parts[2]
+    sys.exit(0)
+
 
 if __name__ == '__main__':
 
-    #testBatch()
+    #testBatchGetLinks()
+
+    (init_urls, gene, rulepathjson) = parseArgs()
+    moreInitUrls = generateMoreInitUrls(init_urls, gene)
+    print moreInitUrls
+    rulepath = parseRulePathParam(rulepathjson)
+    serverDomain = parseServerDomain(init_urls[0])
 
     createDir(saveDir)
-    
-    global grapHtmlPool
-    grapHtmlPool = IoTaskThreadPool(20)
 
-    global dwPicPool
+    grapHtmlPool = IoTaskThreadPool(20)
     dwPicPool = IoTaskThreadPool(20)
 
-    downloadFromPconline(145, 1, 2) 
+    downloadFromUrls(moreInitUrls, rulepath)
     dwPicPool.close()
     dwPicPool.join()
