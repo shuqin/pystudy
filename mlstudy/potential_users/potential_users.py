522bcd2253e4c8783779d9df28ea6d944d9d7541
shuqin <shuqin@youzan.com>
2018-03-10 22:02:11 +0800

    improve kNN algorithm

--- mlstudy/potential_users/potential_users.py
+++ mlstudy/potential_users/potential_users.py
@@ -15,20 +15,22 @@ import matplotlib.pyplot as plot
 from numpy import *
 import operator
 
+indicatorNumber = 3
+
 def file2matrix(filename):
     dataf = open(filename)
     lines = dataf.readlines()
     numOfLines = len(lines)
-    returnMat = zeros((numOfLines, 2))
+    dataMatrix = zeros((numOfLines, indicatorNumber))
     classLabelVector = []
     index = 0
     for line in lines:
         line = line.strip()
         listFromLine = line.split()
-        returnMat[index, :] = listFromLine[0:2]
+        dataMatrix[index, :] = listFromLine[0:indicatorNumber]
         classLabelVector.append(int(listFromLine[-1]))
         index += 1
-    return (returnMat, classLabelVector)
+    return (dataMatrix, classLabelVector)
 
 def autoNorm(dataset):
     minVals = dataset.min(0)
@@ -54,29 +56,44 @@ def classify(inX, dataset, labels, k):
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
-if __name__ == '__main__':
-    (returnMat,classLabelVector) = file2matrix('data.txt')
-    (returnMat,ranges, minVals) = autoNorm(returnMat)
-    print 'dataset: ', (returnMat,classLabelVector)
-
+def draw(data):
     fig = plot.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(returnMat[:, 0], returnMat[:,1])
-    #ax.scatter(returnMat[:, 0], returnMat[:,1], 20.0*array(classLabelVector), 20.0*array(classLabelVector))
+    ax.scatter(data[:, 0], data[:,1])
+    #ax.scatter(data[:, 0], data[:,1], 20.0*array(classLabelVector), 20.0*array(classLabelVector))
     plot.title('potiential renewal users figure')
     plot.xlabel('daily order number')
     plot.ylabel('daily actual deal')
     #plot.show()
 
-    ratio = 0.20
-    totalRows = returnMat.shape[0]
-    testRows = int(totalRows*ratio)
+def computeErrorRatio(dataMatrix, classLabelVector):
+    testRatio = 0.20
+    totalRows = dataMatrix.shape[0]
+    testRows = int(totalRows*testRatio)
     errorCount = 0
     for i in range(testRows):
-        classifierResult = classify(returnMat[i,:], returnMat[testRows:totalRows, :], classLabelVector[testRows:totalRows], 2)
+        classifierResult = classify(dataMatrix[i,:], dataMatrix[testRows:totalRows, :], classLabelVector[testRows:totalRows], 2)
         print 'classify result: %d, the real result is %d' % (classifierResult, classLabelVector[i])
         if classifierResult != classLabelVector[i]:
             errorCount += 1.0
-    print 'total error rate is %f' % (errorCount / float(testRows)) 
+    print 'total error rate is %f' % (errorCount / float(testRows))
 
-       
+def classifyInstance(dataMatrix,classLabelVector, ranges, minVals):
+    dataf = open('data.txt')
+    for line in dataf:
+        line = line.strip()
+        (kid, orderNumber, actualDeal, fans) = map(lambda x: int(x), line.split())
+        input = array([orderNumber, actualDeal, fans])
+        classifierResult = classify((input-minVals)/ranges, dataMatrix, classLabelVector, 2)
+        print '%d [orderNumber=%d actualDeal=%d fans=%d] is %s potiential renewal user' % (kid, orderNumber, actualDeal, fans, "not" if classifierResult != 1 else "" )
+
+if __name__ == '__main__':
+    (dataMatrix,classLabelVector) = file2matrix('sample.txt')
+    (dataMatrix,ranges, minVals) = autoNorm(dataMatrix)
+    
+    print 'dataset: ', (dataMatrix,classLabelVector)
+    draw(dataMatrix)
+
+    computeErrorRatio(dataMatrix, classLabelVector)
+    classifyInstance(dataMatrix,classLabelVector, ranges, minVals)
+
