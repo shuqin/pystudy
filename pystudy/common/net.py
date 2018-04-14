a74036eb0c12b113d8827969ab3b563625169b35
shuqin <shuqin@youzan.com>
2018-04-14 12:17:54 +0800

    模块化的串行图片下载工具

--- pystudy/common/net.py
+++ pystudy/common/net.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
 from common import catchExc
 
 @catchExc
@@ -28,4 +30,21 @@ def batchGetSoups(urls):
     return getUrlPool.map(getSoup, urls)
 
 
+@catchExc
+def download(piclink, saveDir):
+    '''
+       download pic from pic href such as
+            http://img.pconline.com.cn/images/upload/upc/tx/photoblog/1610/21/c9/28691979_1477032141707.jpg
+    '''
 
+    picsrc = piclink.attrs['src']
+    picname = picsrc.rsplit('/',1)[1]
+    saveFile = saveDir + '/' + picname
+
+    picr = requests.get(piclink.attrs['src'], stream=True)
+    with open(saveFile, 'wb') as f:
+        for chunk in picr.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+    f.close()
